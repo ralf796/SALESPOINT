9d26e1adbd400c9f35ada11987ae16df5c3de15e
Raul Lopez <alejandrolopez445@gmail.com>
2023-02-15 20:55:33 -0600

    Se agrega la cotizacion

---
+++ Salespoint/Controllers/VENTAS/VEN_CrearCotizacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salespoint.Controllers.VENTAS
+{
+    public class VEN_CrearCotizacionController : Controller
+    {
+        // GET: VEN_CrearCotizacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
