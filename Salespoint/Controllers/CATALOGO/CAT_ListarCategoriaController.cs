759b0556a9e5fc19867d3ebb5bd82dd635db040a
ralf796 <alejandrolopez445@gmail.com>
2023-04-25 21:40:34 -0600

    Se integra el resta

--- Salespoint/Controllers/CATALOGO/CAT_ListarCategoriaController.cs
+++ Salespoint/Controllers/CATALOGO/CAT_ListarCategoriaController.cs
@@ -137,3 +137,4 @@ namespace Salespoint.Controllers.CATALOGO
         }
     }
 }
+
--- Salespoint/Controllers/Ventas/VEN_CrearVentaController.cs
+++ Salespoint/Controllers/Ventas/VEN_CrearVentaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BE;
+using Salespoint.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
