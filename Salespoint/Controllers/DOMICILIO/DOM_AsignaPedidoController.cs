37791c7f3a70855554a462d6813163a8d6f50ede
Raul Lopez <alejandrolopez445@gmail.com>
2023-06-05 18:01:26 -0600

    feat(Pedidos): Se agrega pantalla para pedidos a domicilio y recoger en tienda

---
+++ Salespoint/Controllers/DOMICILIO/DOM_AsignaPedidoController.cs
@@ -0,0 +1,34 @@
+﻿using HELPERS;
+using RESTAURANTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salespoint.Controllers.DOMICILIO
+{
+    public class DOM_AsignaPedidoController : Controller
+    {
+        // GET: DOM_AsignaPedido
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [SessionExpireFilter]
+        public PartialViewResult Get_PedidosPendientesDeEntregar()
+        {
+            var item = new Pedido_BE();
+            item.MTIPO = 15;
+            item.CREADO_POR = Session["usuario"].ToString();
+
+            List<Pedido_BE> lista_encabezado;
+            List<Pedido_BE> lista_detalle;
+            lista_encabezado = Connect.Connect_Pedido(item);
+
+            ViewBag.data_encabezado = lista_encabezado;
+            return PartialView();
+        }
+    }
+}
---
+++ Salespoint/Controllers/VENTAS/PED_PedidoCajaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salespoint.Controllers.VENTAS
+{
+    public class PED_PedidoCajaController : Controller
+    {
+        // GET: PED_PedidoCaja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
